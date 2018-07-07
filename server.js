33d0e11bf67958d944515ea8cd5db46ca7ddc515
Alex <saneksa2@gmail.com>
2018-07-07 19:37:08 +0300

    fix

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require('express')
-const app = express()
-const port = 4000
-const data = require('./document.json')
+var express = require('express')
+var app = express()
+var port = 4000
+var data = require('./document.json')
 var cors = require('cors')
 
 app.use(cors())
@@ -13,5 +13,5 @@ app.listen(port, function(err) {
     if (err) {
         return console.log('something bad happened', err)
     }
-    return console.log(`server is listening on ${port}`)
+    return console.log('server is listening on ', port)
 })
