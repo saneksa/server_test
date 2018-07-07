e79100a71bf269bd1ed052e38578cfe5dc505608
Alex <saneksa2@gmail.com>
2018-07-07 21:11:31 +0300

    fix

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 var express = require('express')
 var app = express()
-var port = 4000
+var port = process.env.PORT || 4000
 var data = require('./document.json')
 
-app.get('/data', function(request, response) {
+app.use(function (req, res, next) {
+    res.header('Content-Type', 'application/json');
+    next();
+});
+
+app.get('/', function(request, response) {
    return response.json({dsf: 'dffdfd'})
 })
 
